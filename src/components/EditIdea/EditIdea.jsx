f77be273e66110297bb951d79fef57d31eaecb08
John Maybee <john.d.maybee@gmail.com>
2023-03-08 16:35:37 -0500

    made updates to the EditIdea page, so that the different properties and their attributes are more readable. Also, mapped thisIdea more cleanly inside of the form. Then created a save/update button that updates the different parameters by setting them to the updated state.

--- src/components/EditIdea/EditIdea.jsx
+++ src/components/EditIdea/EditIdea.jsx
@@ -4,36 +4,57 @@ import React, { useState, useEffect } from 'react';
 import './EditIdea.css';
 
 
-function EditIdea() {
-    const user = useSelector((store) => store.user);
-    const idea = useSelector((store) => store.thisIdea);
-
-
-    const [name, setName] = useState(idea.name);
-    const [details, setDetails] = useState(idea.details);
-    const [style, setStyle] = useState(idea.style); // should I be putting this getter value somwhere in the return?
-    const [placement, setPlacement] = useState(idea.placement); // should I be putting this getter value somwhere in the return?
-    const { id } = useParams();
-
+const EditIdea = () =>{
     const dispatch = useDispatch();
     const history = useHistory();
+    
+    const user = useSelector((store) => store.user);
+    const thisIdea = useSelector((store) => store.thisIdea);
+    
+    const { id } = useParams();
+    const [name, setName] = useState([{name:''}]);
+    const [details, setDetails] = useState([{details:''}]);
+    const [style, setStyle] = useState([{styel:''}]); // should I be putting this getter value somwhere in the return?
+    const [placement, setPlacement] = useState([{placement:''}]); // should I be putting this getter value somwhere in the return?
+
     console.log('asdfghjdfghjdfghj', id);
 
     useEffect(() => {
         dispatch({
-            type: 'FETCH_THIS_IDEA',
+            type: "FETCH_THIS_IDEA",
             payload: id
         });
-    }, []);
+    }, [id]);
+
+    useEffect(() => {
+        setName(thisIdea.name);
+        setDetails(thisIdea.details);
+        setStyle(thisIdea.style);
+        setPlacement(thisIdea.placement);
+    }, [thisIdea]);
+
+    const updateTattooIdea = () => {
+        const nameUpdate = [...name, {name: ''}];
+        const detailsUpdate = [...details, {details: ''}];
+        const styleUpdate = [...style, {style: ''}];
+        const placementUpdate = [...placement, {placement: ''}];
+        setName(nameUpdate);
+        setDetails(detailsUpdate);
+        setStyle(styleUpdate);
+        setPlacement(placementUpdate);
+    }
 
     const submitUpdate = (event) => {
         event.preventDefault();
+        console.log('submitting the payload: ', id, name, details, style, placement);
           dispatch({
-            type: 'EDIT_IDEA',
+            type: "EDIT_IDEA",
             payload: {id, name, details, style, placement}
-          }); 
-          // history.push back to the ideas page
-          history.push('/ideas');
+          });
+          // should I set each param back to ''? 
+          // or does clicking the edit idea button on the ideas page change this page on load anyway?
+          // history.back to the ideas page/previous page
+          history.goBack();
     };
     
 
@@ -45,90 +66,117 @@ function EditIdea() {
             </section>
 
             <section className="edit-idea-container">
-                {idea.map(idea => {
-                    return(
-                        <div key={idea.id}>
-                            
-                        <form className="edit-idea-form" onSubmit={submitUpdate}>
+                        <div className="edit-idea">
+                            <form onSubmit={submitUpdate}>
+                                  
+                            {thisIdea.map((thisIdea) => {
+                                return (
+                                    <div key={thisIdea.id} className="edit-idea">
+                                            <label htmlFor="name">Tattoo Name: {thisIdea.name}</label><br/>
+                                            <input 
+                                                key={thisIdea.name}
+                                                value={name}
+                                                id="name"
+                                                placeholder="Update Name"
+                                                onChange={(event) => setName(event.target.value)} 
+                                            />
+                                            <br/>
 
-                            <section className="edit-idea-name">
-                                <label htmlFor="name">Tattoo Name: </label>
-                                <br/>
-                                <input value={name} id="name" placeholder={idea.name} onChange={(event) => setName(event.target.value)}/>
-                            </section>
-                    
-                            <section className="edit-idea-details">
-                                <label htmlFor="details">Details: </label>
-                                <br/>
-                                <input value={details} id="details" placeholder={idea.details} onChange={(event) => setDetails(event.target.value)} />
-                            </section>
-                    
-                            <section className="edit-idea-style">
-                                <label htmlFor="style">Style [choose one]:</label>
-                                &nbsp; &nbsp;
-                                <select value={style} onChange={(event) => setStyle(event.target.value)}>
-                                    <option value={idea.style}>{idea.style}</option>
-                                    <option value="Undecided">Undecided</option>
-                                    <option value="American Traditional">American Traditional</option>
-                                    <option value="Black and Grey">Black and Grey</option>
-                                    <option value="Neo-Traditional">Neo-Traditional</option>
-                                    <option value="Realism">Realism</option>
-                                    <option value="New School">New School</option>
-                                    <option value="Fine Line">Fine Line</option>
-                                    <option value="Japanese Traditional">Japanese Traditional</option>
-                                    <option value="Tribal">Tribal</option>
-                                    <option value="Illustrative">Illustrative</option>
-                                    <option value="Ornamental">Ornamental</option>
-                                    <option value="Abstract">Abstract</option>
-                                    <option value="Blackwork">Blackwork</option>
-                                    <option value="Cartoon/Anime">Cartoon/Anime</option>
-                                    <option value="Continuous Line Contour">Continuous Line Contour</option>
-                                    <option value="Geometric">Geometric</option>
-                                    <option value="Script/Lettering">Script/Lettering</option>
-                                    <option value="Surrealism">Surrealism</option>
-                                    <option value="Trash Polka">Trash Polka</option>
-                                    <option value="Watercolor">Watercolor</option>
-                                </select>
-                            </section>
-                    
-                            <section className="edit-idea-placement">
-                                <label htmlFor="placement">Desired Placement [choose one]:</label>
-                                &nbsp; &nbsp;
-                                <select value={placement} onChange={(event) => setPlacement(event.target.value)}>
-                                    <option value={idea.placement}>{idea.placement}</option>
-                                    <option value="Undecided">Undecided</option>
-                                    <option value="Upper Arm - Right">Upper Arm - Right</option>
-                                    <option value="Upper Arm - Left">Upper Arm - Left</option>
-                                    <option value="Forearm - Right">Forearm - Right</option>
-                                    <option value="Forearm - Left">Forearm - Left</option>
-                                    <option value="Upper Leg - Right">Upper Leg - Right</option>
-                                    <option value="Upper Leg - Left">Upper Leg - Left</option>
-                                    <option value="Lower Leg - Right">Lower Leg - Right</option>
-                                    <option value="Lower Leg - Left">Lower Leg - Left</option>
-                                    <option value="Side - Right">Side - Right</option>
-                                    <option value="Side - Left">Side - Left</option>
-                                    <option value="Back">Back</option>
-                                    <option value="Chest">Chest</option>
-                                    <option value="Abdomen">Abdomen</option>
-                                    <option value="Hand - Right">Hand - Right</option>
-                                    <option value="Hand - Left">Hand - Left</option>
-                                    <option value="Foot - Right">Foot - Right</option>
-                                    <option value="Foot - Left">Foot - Left</option>
-                                    <option value="Neck">Neck</option>
-                                    <option value="Face">Face</option>
-                                    <option value="Butt">Butt</option>
-                                    <option value="Head">Head</option>
-                                </select>
-                            </section>
-                            <br />
-                            
-                            <button className="submit-button" type="submit">Update Idea</button>
-                    
-                        </form>
-                        
+                                            <label htmlFor="details">Details: {thisIdea.details}</label><br/>
+                                            <input 
+                                                key={thisIdea.details}
+                                                value={details} id="details"
+                                                placeholder="Update Details"
+                                                onChange={(event) => setDetails(event.target.value)}
+                                            />                         
+                                            <br/>
+
+                                            <label htmlFor="style">Style [choose one]:</label>
+                                            &nbsp; &nbsp;
+                                            <select 
+                                                key={thisIdea.style}
+                                                value={style}
+                                                onChange={(event) => setStyle(event.target.value)}
+                                            >
+                                                <option value={thisIdea.style}>{thisIdea.style}</option>
+                                                <option value="Undecided">Undecided</option>
+                                                <option value="American Traditional">American Traditional</option>
+                                                <option value="Black and Grey">Black and Grey</option>
+                                                <option value="Neo-Traditional">Neo-Traditional</option>
+                                                <option value="Realism">Realism</option>
+                                                <option value="New School">New School</option>
+                                                <option value="Fine Line">Fine Line</option>
+                                                <option value="Japanese Traditional">Japanese Traditional</option>
+                                                <option value="Tribal">Tribal</option>
+                                                <option value="Illustrative">Illustrative</option>
+                                                <option value="Ornamental">Ornamental</option>
+                                                <option value="Abstract">Abstract</option>
+                                                <option value="Blackwork">Blackwork</option>
+                                                <option value="Cartoon/Anime">Cartoon/Anime</option>
+                                                <option value="Continuous Line Contour">Continuous Line Contour</option>
+                                                <option value="Geometric">Geometric</option>
+                                                <option value="Script/Lettering">Script/Lettering</option>
+                                                <option value="Surrealism">Surrealism</option>
+                                                <option value="Trash Polka">Trash Polka</option>
+                                                <option value="Watercolor">Watercolor</option>
+                                            </select>
+                                            <br/>
+
+                                            <label htmlFor="placement">Desired Placement [choose one]:</label>
+                                            &nbsp; &nbsp;
+                                            <select 
+                                                key={thisIdea.placement}
+                                                value={placement}
+                                                onChange={(event) => setPlacement(event.target.value)}
+                                            >
+                                                <option value={thisIdea.placement}>{thisIdea.placement}</option>
+                                                <option value="Undecided">Undecided</option>
+                                                <option value="Upper Arm - Right">Upper Arm - Right</option>
+                                                <option value="Upper Arm - Left">Upper Arm - Left</option>
+                                                <option value="Forearm - Right">Forearm - Right</option>
+                                                <option value="Forearm - Left">Forearm - Left</option>
+                                                <option value="Upper Leg - Right">Upper Leg - Right</option>
+                                                <option value="Upper Leg - Left">Upper Leg - Left</option>
+                                                <option value="Lower Leg - Right">Lower Leg - Right</option>
+                                                <option value="Lower Leg - Left">Lower Leg - Left</option>
+                                                <option value="Side - Right">Side - Right</option>
+                                                <option value="Side - Left">Side - Left</option>
+                                                <option value="Back">Back</option>
+                                                <option value="Chest">Chest</option>
+                                                <option value="Abdomen">Abdomen</option>
+                                                <option value="Hand - Right">Hand - Right</option>
+                                                <option value="Hand - Left">Hand - Left</option>
+                                                <option value="Foot - Right">Foot - Right</option>
+                                                <option value="Foot - Left">Foot - Left</option>
+                                                <option value="Neck">Neck</option>
+                                                <option value="Face">Face</option>
+                                                <option value="Butt">Butt</option>
+                                                <option value="Head">Head</option>
+                                            </select>
+                                        
+                                        <br />
+                                        </div>
+                                        )
+                            })}
+                                <button 
+                                    onClick={updateTattooIdea}
+                                    type="button"
+                                    className="save-button"
+                                >
+                                    Save Changes
+                                </button>
+
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+                                <button 
+                                    className="submit-button"
+                                    type="submit"
+                                >
+                                    Send update to my tattoos
+                                </button>
+
+                            </form>  
                         </div>
-                    )
-                })}
             </section>
         </div>
       );
