8cd63e14074386be6bea5cbe90d4a02ff24d4550
John Maybee <john.d.maybee@gmail.com>
2023-03-11 19:55:42 -0500

    Updated the buttons on the UserPage. They are now mui buttons that have links within them and also contain text. Removed the text decoration via the App.css styling component.

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,19 +1,56 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import {useSelector} from 'react-redux';
+import { Button } from '@mui/material';
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
   return (
     <div className="container">
-      <h2 className="header_pad" >{user.username}'s Ink Pad</h2>
-      <Link to="/create"><button className="btn_stretched">Create New Tattoo</button></Link>
+
+      <h2 className="header_pad" >
+        {user.username}'s Ink Pad
+      </h2>
+
+      <Button 
+        className="mui_btn" 
+        variant="contained" 
+        sx={{ 
+          border: "1px solid #282828", 
+          backgroundColor: '#E1AD01', 
+          color: "#282828", 
+          width: "100%", 
+          "&:active": {backgroundColor: "#fcf7e6"}, 
+          "&:hover": {backgroundColor:"#80a9a4"} 
+        }}>
+          <Link className="mui_link" to="/create">
+            Create New Tattoo
+          </Link>
+      </Button>
+
       <br />
       <br />
-      <Link to="/ideas"><button className="btn_stretched">My Tattoo Ideas</button></Link>
-      <br />
-      <br />
+
+      <Button 
+        className="mui_btn" 
+        variant="contained" 
+        sx={{ 
+          border: "1px solid #282828", 
+          backgroundColor: '#E1AD01', 
+          color: "#282828", 
+          width: "100%", 
+          "&:active": {backgroundColor: "#fcf7e6"}, 
+          "&:hover": {backgroundColor:"#80a9a4"} 
+        }}>
+          <Link className="mui_link" to="/ideas">
+            My Tattoo Ideas
+          </Link>
+      </Button>
+
+      {/* <br />
+      <br /> */}
+      
     </div>
   );
 }
@@ -21,5 +58,4 @@ function UserPage() {
 // this allows us to use <App /> in index.js
 export default UserPage;
 
-// is this where we are supposed to enter in the home page information?
-// Or do I need a seperate component for the home page?
+// Have two commented out breaks that will be used if the stretch goal of adding artists is reached.
