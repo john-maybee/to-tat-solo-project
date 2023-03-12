fdca732a14104f97dfaa1cb681d768fedaa54ad4
John Maybee <john.d.maybee@gmail.com>
2023-03-12 14:36:57 -0400

    imported the artists reducer into the root.reducer.

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -3,6 +3,7 @@ import errors from './errors.reducer';
 import user from './user.reducer';
 import ideas from './ideas.reducer';
 import thisIdea from './this.idea.reducer';
+import artists from './artists.reducer';
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
@@ -15,6 +16,7 @@ const rootReducer = combineReducers({
   user, // will have an id and username if someone is logged in
   ideas,
   thisIdea, 
+  artists,
 });
 
 export default rootReducer;
